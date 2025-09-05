fb97d8ea2c77a8a4a8a3fe70d0f0072552b5a51b
Huyratio <44249954+huyratio5998@users.noreply.github.com>
2025-09-05 06:05:18 +0100

    update gallary feature

--- js/script.js
+++ js/script.js
@@ -63,7 +63,17 @@
         type: "image",
         gallery: {
           enabled: true,
+          preload: [0,2],
+          keyboard: true,          
         },
+        zoom: {
+          enabled: true,
+          duration: 300,
+          easing: 'ease-in-out',
+          opener: function(openerElement) {
+            return openerElement.is('img') ? openerElement : openerElement.find('img');
+          }
+        }
       });
   });
 
